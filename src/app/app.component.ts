fca4d3e463fe68055ffdb779be7285a201e30748
Nevalainen Susanna <susanna.nevalainen@aalto.fi>
2018-02-19 21:22:58 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+interface UserResponse {
+  login: string,
+  bio: string
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private http: HttpClient){
+
+  }
+
+  ngOnInit(): void {
+    this.http.get<UserResponse>('https://api.github.com/users/zuzze').subscribe(
+      data => {
+      console.log("User Login: " + data.login);
+    }, 
+  (err : HttpErrorResponse) => {
+    if(err.error instanceof Error){
+      console.log("Client-side error occured");
+    }
+    console.log("server-side error occured");
+  });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
+import { HttpClient } from '@angular/common/http/src/client';
 
 
 @NgModule({
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
